3f9337c3649d15a4d5849c5ed1e8c50b83c90c86
ninjahw <celest.angela@gmail.com>
2024-09-17 00:23:11 +0800

    add Products component and 4 products pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,11 @@
-import { Button } from "@/components/ui/button";
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar"
-
-import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/ui/Navbar";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <div className='justify-center'>
-      <Navbar></Navbar>
+      <Navbar/>
 
-      {/* <Menubar>
-          <MenubarMenu>
-            <MenubarTrigger>File</MenubarTrigger>
-            <MenubarContent>
-              <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>New Window</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Share</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Print</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
-
-        <Button>Click here</Button> */}
-
-      
       <Footer />
     </div>
   );
