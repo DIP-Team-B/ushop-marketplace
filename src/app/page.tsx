ea3b01483d26a6f1249047d3c10031c29bc1eb92
Elvin Sugianto <elvinsugianto@Elvins-MacBook-Air.local>
2024-09-24 20:42:22 +0800

    adding promotion banner template and fixing navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import HomepageCategoryCards from "@/components/HomepageCategoryCard";
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Home() {
   const categoryItems = [
@@ -26,24 +34,41 @@ export default function Home() {
         
       </div> */}
       {/* screen size */}
-      <div className="screen-size-wrapper w-screen px-40 py-8 gap-6 flex flex-col">
+      <div className="screen-size-wrapper w-screen px-40 py-8 gap-6 flex flex-col items-center">
         {/* promotion */}
+        <Carousel className="w-full h-64 relative">
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card className="h-64">
+                    <CardContent className="flex w-full h-full items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="absolute left-6" />
+          <CarouselNext className="absolute right-6" />
+        </Carousel>
 
         {/* category cards */}
-        <div className="flex flex-col gap-3 items-center">
-          <p className="font-semibold text-xl">#ShopByMood</p>
+        <div className="flex flex-col gap-2 w-full items-center pt-6">
+          <p className="font-semibold text-xl text-mainBlack">#ShopByMood</p>
+          <div className="flex flex-col lg:flex-row p-4 gap-4 bg-secondBG w-full rounded-md items-center">
+            {categoryItems.map((item) => (
+              <HomepageCategoryCards
+                key={item.id}
+                title={item.title}
+                id={item.id}
+              />
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col lg:flex-row p-4 gap-4 bg-secondBG w-full rounded-md items-center">
-          {categoryItems.map((item) => (
-            <HomepageCategoryCards
-              key={item.id}
-              title={item.title}
-              id={item.id}
-            />
-          ))}
-        </div>
-
-
       </div>
       <Footer />
     </div>
