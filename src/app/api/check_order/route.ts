2e056492288de78920212193da89d717cdf006a2
kaii-99 <chuakaiwei2012@gmail.com>
2024-10-25 21:48:22 +0800

    25/10/2024

--- src/app/api/check_order/route.ts
+++ src/app/api/check_order/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { createConnection } from "c:/ushop-marketplace/src/lib/db";
+import { createConnection } from '@/lib/db';
 
 export async function GET(request: Request) {
   let connection;
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,17 +1,13 @@
-import { createConnection as mysqlCreateConnection } from "mysql2/promise";
-
-// Load environment variables from .env file
-require('dotenv').config();
+import mysql from "mysql2/promise";
 
 // Create a function to establish a MySQL connection
 export const createConnection = async () => {
   try {
-    const connection = await mysqlCreateConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      charset: 'utf8mb4', // Optional: add charset for better encoding
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'ntushop'
     });
     console.log("Database connection successful");
     return connection;
